22b853999e3f066323aedab26f712fe3fb3d2f00
Sonali380 <146807945+Sonali380@users.noreply.github.com>
2023-11-22 14:06:01 +0530

    Update streamlit_app.py
    Create a Function

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,6 @@ import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
-
-
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kela, Spinach & Rocket Smoothie')
@@ -14,10 +12,6 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#streamlit.write('The user entered ', fruit_choice)
-
-#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -27,16 +21,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
+# create  the repeatable code block(Called a function)
+def get_fruityvice_data(this_fruit_choice):
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruite_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      return fruityvice_normalized
+  
+#New section to display
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get informartion.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruite_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function=get_fruityvice_data(fruit_choice)  
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
   
